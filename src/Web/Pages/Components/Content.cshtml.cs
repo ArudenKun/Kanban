829aa588750691e1a94855b071564f9600b02687
Aruden <84431560+ArudenKun@users.noreply.github.com>
2025-03-17 05:10:29 +0800

    Added clean architecture

---
+++ src/Web/Pages/Components/Content.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using Hydro;
+
+namespace Web.Pages.Components;
+
+public class Content : HydroComponent
+{
+    public int Count { get; set; }
+
+    public void Add()
+    {
+        Count++;
+        Client.ExecuteJs(
+            """
+            console.log('Test')
+            """
+        );
+    }
+}
